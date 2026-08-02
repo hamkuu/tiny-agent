3b4f27c0fd99ac61d3fc1083b09e7fa7b508ded7
Hiro Shaw <hamkuu@gmail.com>
2026-08-02 20:21:28 +0900

    use native tool

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ from agent import TinyAgent
 from llm import LLM
 from memory import Memory
 from toolbox import multiply
-from tools import Tools, NativeTools
+from tools import NativeTools
 
-llm = LLM(model="gemma3:12b")
+llm = LLM(model="gemma4:e4b")
 memory = Memory()
 
-tools = Tools(requires_approval=[])
-tools.add_tool(
-    name="multiply",
-    func=multiply,
-    description="Multiplies two numbers: multiply(a: str, b: str)",
-)
+tools = NativeTools(requires_approval=[])
+tools.add_tool("multiply", multiply)
 
 agent = TinyAgent(llm=llm, memory=memory, tools=tools)
 
